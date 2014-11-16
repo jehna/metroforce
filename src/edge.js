8762fb47f7248868a48400312989b105e6d7260c
jehna <jesse.luoto@gmail.com>
2014-11-16 20:17:15 +0200

    Fixed: Edge not to create duplicates on render

--- src/edge.js
+++ src/edge.js
@@ -2,22 +2,21 @@ function Edge(from,to,label) {
     this.from = from;
     this.to = to;
     this.label = label;
+    
     from.edges.push(this);
     to.edges.push(this);
+    
+    this.element = Utils.createSVGElem("line", edgesGroup, {
+        class: this.label ? this.label.className : "unlabeled"
+    });
 }
 
 Edge.prototype.render = function() {
     
-    var line = document.createElementNS(xmlns, "line");
-    line.setAttribute("x1", this.from.x);
-    line.setAttribute("y1", this.from.y);
-    line.setAttribute("x2", this.to.x);
-    line.setAttribute("y2", this.to.y);
+    this.element.setAttribute("x1", this.from.x);
+    this.element.setAttribute("y1", this.from.y);
+    this.element.setAttribute("x2", this.to.x);
+    this.element.setAttribute("y2", this.to.y);
     
-    if (this.label) {
-        line.setAttribute("class", this.label.className);
-    }
-    
-    edgesGroup.appendChild(line);
     return this;
 }
